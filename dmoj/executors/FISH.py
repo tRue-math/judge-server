3a3fbd5d16acf8748e0e7643ef14913dc3c836ca
platypus <platypus.computerchip@gmail.com>
2025-04-27 16:35:08 +0900

    Modify FISH.py

--- dmoj/executors/FISH.py
+++ dmoj/executors/FISH.py
@@ -1,11 +1,19 @@
 from dmoj.cptbox.filesystem_policies import RecursiveDir
 from dmoj.executors.script_executor import ScriptExecutor
 
+from typing import List
 
 class Executor(ScriptExecutor):
     ext = 'fish'
     command = 'fish'
-    fs = [RecursiveDir('~')]
     test_program = """\
 "!dlrow ,olleH"l?!;oe0.
 """
+
+    syscalls = ['vfork']
+
+    def get_cmdline(self, **kwargs) -> List[str]:
+        command = self.get_command()
+        assert command is not None
+        assert self._code is not None
+        return [command, self._code]
