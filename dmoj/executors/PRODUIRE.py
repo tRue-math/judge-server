848792937f1830ca06ee24f261f6e76ad164165f
tRue <hi06yo13ri@gmail.com>
2025-05-11 00:37:47 +0900

    Fix PRODUIRE.py

--- dmoj/executors/PRODUIRE.py
+++ dmoj/executors/PRODUIRE.py
@@ -1,18 +1,33 @@
-from typing import List
+from dmoj.executors.script_executor import ScriptExecutor
 
-from dmoj.executors.mono_executor import MonoExecutor
+from dmoj.cptbox import TracedPopen
+from dmoj.cptbox.filesystem_policies import RecursiveDir
+from dmoj.cptbox.handlers import ACCESS_EAGAIN
 
-class Executor(MonoExecutor):
+
+class MonoTracedPopen(TracedPopen):
+    def _cpu_time_exceeded(self) -> None:
+        pass
+
+class Executor(ScriptExecutor):
     ext = 'produire'
     command = 'produire'
 
     test_program = """\
 ｢echo: Hello, World!｣を報告
 """
-    syscalls = ['execve','unlink','wait4','clock_nanosleep','fork']
-
-    def get_compile_args(self) -> List[str]:
-        command = self.get_command()
-        assert command is not None
-        assert self._code is not None
-        return [command, self._code, f'-out:{self.get_compiled_file()}']
+    
+    nproc = -1
+    address_grace = 262144
+    data_grace = 65536
+    cptbox_popen_class = MonoTracedPopen
+    fs = [RecursiveDir('/etc/mono')]
+    syscalls = [
+        'wait4',
+        'rt_sigsuspend',
+        'msync',
+        'fadvise64',
+        'clock_nanosleep',
+        ('fork', ACCESS_EAGAIN),
+        'execve','fstatfs','chdir'
+    ]
