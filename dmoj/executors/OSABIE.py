3d2a7245efe3297a8ad29d1d2326a83de62e5540
tRue <hi06yo13ri@gmail.com>
2025-05-13 00:28:16 +0900

    Fix OSABIE.py

--- dmoj/executors/OSABIE.py
+++ dmoj/executors/OSABIE.py
@@ -6,3 +6,8 @@ class Executor(ScriptExecutor):
     test_program = """\
 "echo: Hello, World!
 """
+    nproc = -1
+    address_grace = 5000000
+    data_grace = 1000000
+    syscalls = ['execve','wait4','memfd_create','ftruncate','socketpair','setsid','pselect6',
+                'timerfd_settime','readv','getsockopt']
