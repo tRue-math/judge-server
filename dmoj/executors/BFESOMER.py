5d17b6f5b8dbbe5032a1b39e7559f80f0c731026
tRue <hi06yo13ri@gmail.com>
2025-05-12 22:51:47 +0900

    Fix BFESOMER.py
    it doesn't work

--- dmoj/executors/BFESOMER.py
+++ dmoj/executors/BFESOMER.py
@@ -1,5 +1,6 @@
 from typing import List
 
+from dmoj.cptbox.filesystem_policies import ExactFile
 from dmoj.executors.compiled_executor import CompiledExecutor
 
 class Executor(CompiledExecutor):
@@ -8,6 +9,13 @@ class Executor(CompiledExecutor):
     test_program = """,+[-.,+]"""
 
     syscalls = ['vfork']
+
+    compiler_read_fs = [
+        ExactFile('/var/lib/gems/3.3.0/gems/esomer-0.1.0/lib/esomer.rb')
+    ]
+    compiler_write_fs = [
+        ExactFile('/var/lib/gems/3.3.0/gems/esomer-0.1.0/lib/esomer.rb')        
+    ]
 
     def get_compile_args(self) -> List[str]:
         command = self.get_command()
