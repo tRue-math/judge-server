785078f5fe72c1149b7ea425b21ae9f3a0a14b87
tRue <hi06yo13ri@gmail.com>
2025-10-14 21:31:28 +0900

    Shoreten PRDR.py

--- dmoj/executors/PRDR.py
+++ dmoj/executors/PRDR.py
@@ -1,13 +1,7 @@
 from dmoj.executors.script_executor import ScriptExecutor
 
-from dmoj.cptbox import TracedPopen
 from dmoj.cptbox.filesystem_policies import RecursiveDir
 from dmoj.cptbox.handlers import ACCESS_EAGAIN
-
-
-class MonoTracedPopen(TracedPopen):
-    def _cpu_time_exceeded(self) -> None:
-        pass
 
 class Executor(ScriptExecutor):
     ext = 'rdr'
@@ -18,9 +12,6 @@ class Executor(ScriptExecutor):
 """
     
     nproc = -1
-    address_grace = 262144
-    data_grace = 65536
-    cptbox_popen_class = MonoTracedPopen
     fs = [RecursiveDir('/etc/mono')]
     syscalls = [
         'wait4',
