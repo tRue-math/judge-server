f142adb685c7f527575b24ebb9a0e3ffb340070e
Taisei-dev <87258462+Taisei-dev@users.noreply.github.com>
2025-11-06 11:08:01 +0900

    Add support for Grass

---
+++ dmoj/executors/GRASS.py
@@ -0,0 +1,6 @@
+from dmoj.executors.script_executor import ScriptExecutor
+
+class Executor(ScriptExecutor):
+    ext = 'grass'
+    command = 'grass'
+    test_program = "wwWWWwwWWwwvwwWWwWWWwWWWWwvwwWWWwwWWWWwWwwwvWwwwwwWwwwwwwwvwWWWWWWWWWWwwWwwwWwWwwwwwwwwwWwwwwWwwwwww"
